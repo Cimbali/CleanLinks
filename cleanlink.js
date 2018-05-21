Cimbali/CleanLinks

commit b8c1b7a3bd03c452fc6f79d0b3c7ed08f1a2fd62

Load config numbers as int, stop using String.trim
String.trim is deprecated
Loading numbers allows to correct additions (otherwise they are strings
that concatenate)

Also fix 2 random bugs:
- use correct pref instead of missing cleanOnClick value
- fix a remaning console.log() -> log()

cleanlink.js
@@ -89,7 +89,9 @@ function loadOptions()
 	{
 		if ('configuration' in data) {
 			for (var param in data.configuration) {
-				if (typeof prefValues[param] != 'object')
+				if (typeof prefValues[param] == 'number')
+					prefValues[param] = parseInt(data.configuration[param]);
+				else if (typeof prefValues[param] == 'string')
 					prefValues[param] = data.configuration[param];
 				else if (prefValues[param] instanceof RegExp)
 					prefValues[param] = new RegExp(data.configuration[param]);
@@ -106,7 +108,7 @@ function highlightLink(node, remove)
 	// parse and apply ;-separated list of key:val style properties
 	('' + prefValues.hlstyle).split(';').forEach(function (r)
 	{
-		let [prop, val] = r.split(':').map(String.trim);
+		let [prop, val] = r.split(':').map(s => s.trim());
 		node.style.setProperty(prop, remove ? '' : val, 'important');
 	});
 }
@@ -215,7 +217,7 @@ function cleanLink(link, base)
 		if ((pos = link.indexOf('&')) !== -1)
 			link = link.substr(0, pos);
 		link = decodeURIComponent(link);
-		console.log('decoded URI Component =', link)
+		log('decoded URI Component =', link)
 
 		if ((pos = link.indexOf('html&')) !== -1 || (pos = link.indexOf('html%')) !== -1)
 			link = link.substr(0, pos + 4);
@@ -254,7 +256,7 @@ function cleanLink(link, base)
 			ht = link.substr(pos), link = link.substr(0, pos);
 
 		link = link.replace(/&amp;/g, '&').replace(prefValues.remove, '').replace(/[?&]$/, '')
-			+ (ht && /^[\w\/#!-]+$/.test(ht) ? ht : (this.cleanOnClick ? '' : '#'));
+			+ (ht && /^[\w\/#!-]+$/.test(ht) ? ht : (prefValues.evdm ? '' : '#'));
 	}
 
 	log('cleaning', origLink, ':', link)

commit 28d9502079f39525c10d03e4b7e343472ed4a73a

Correctly parse boolean options in loadOptions()
Regression introduced in b8c1b7a

cleanlink.js
@@ -92,6 +92,12 @@ function loadOptions()
 			for (var param in data.configuration) {
 				if (typeof prefValues[param] == 'number')
 					prefValues[param] = parseInt(data.configuration[param]);
+				else if (typeof prefValues[param] == 'boolean')
+				{
+					prefValues[param] = data.configuration[param] === true
+									|| data.configuration[param] === 'true'
+									|| data.configuration[param] === 'on';
+				}
 				else if (typeof prefValues[param] == 'string')
 					prefValues[param] = data.configuration[param];
 				else if (prefValues[param] instanceof RegExp)

options.html
@@ -81,7 +81,7 @@
 		<input type="checkbox" name="textcl" value="" />
 	</label>
 	<label><span i18n_text="cltrack_title"></span>
-		<input type="checkbox" name="cltrack" />
+		<input type="checkbox" name="cltrack" value="" />
 	</label>
 	<label i18n_title="repdelay_tooltip"><span i18n_text="repdelay_title"></span>
 		<input type="number" name="repdelay" value="" />