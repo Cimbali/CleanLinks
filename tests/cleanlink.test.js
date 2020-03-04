5b7eed286664f9f8a9c8dcd158eb87cad6c83786
Cimbali <me@cimba.li>
2020-03-04 20:50:50 +0100

    Handle AWS signin
    closes #50

--- tests/cleanlink.test.js
+++ tests/cleanlink.test.js
@@ -95,7 +95,12 @@ describe('cleanLink', function() {
 			),
 		])
 	);
+	it('should allow logging in to AWS', () =>
+	{
+		let url = 'https://signin.aws.amazon.com/signin?redirect_uri=https%3A%2F%2Fconsole.aws.amazon.com%2Fconsole%2Fhome%3Fstate%3DhashArgs%2523%26isauthcode%3Dtrue&client_id=arn%3Aaws%3Aiam%3A%3A015428540659%3Auser%2Fhomepage&forceMobileApp=0'
 
+		return cleanLink(url).then(result => expect(result).to.equal(url))
+	});
 	it('should clean BBC campaign tracking parameters', () =>
 		cleanLink('https://www.bbc.com/news/world-latin-america-45982501?ocid=socialflow_facebook&ns_source=facebook&ns_mchannel=social&ns_campaign=bbcnews').then(result =>
 			expect(result).to.equal('https://www.bbc.com/news/world-latin-america-45982501')
