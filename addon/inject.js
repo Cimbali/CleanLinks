Cimbali/CleanLinks

commit e204c5b5e23d17a3829a8007b045b0ce81acd6fb

Ensure to handle errors that may occur with messages

addon/inject.js
@@ -115,11 +115,11 @@ function on_click(evt)
 	try
 	{
 		let url = new URL(text_link, window.location)
-		browser.runtime.sendMessage({action: 'highlight', link: url.href})
+		browser.runtime.sendMessage({action: 'highlight', link: url.href}).catch(() => {});
 	}
 	catch (e)
 	{
-		browser.runtime.sendMessage({action: 'highlight', link: text_link})
+		browser.runtime.sendMessage({action: 'highlight', link: text_link}).catch(() => {});
 	}
 
 
@@ -131,7 +131,7 @@ function on_click(evt)
 	if (event_do_click(cleaned_link, node, evt))
 	{
 		// instead of blinking the URL bar, tell the background to show a notification.
-		browser.runtime.sendMessage({action: 'notify', url: cleaned_link, orig: text_link, type: 'clicked'});
+		browser.runtime.sendMessage({action: 'notify', url: cleaned_link, orig: text_link, type: 'clicked'}).catch(() => {});
 	}
 }
 
@@ -152,7 +152,9 @@ function toggle_active(enabled)
 		window.removeEventListener('click', on_click, {capture: true});
 }
 
-browser.runtime.sendMessage({action: 'check tab enabled'}).then(answer => toggle_active(answer.enabled));
+browser.runtime.sendMessage({action: 'check tab enabled'})
+	.then(answer => enabled !== undefined && toggle_active(answer.enabled))
+	.catch(() => {});
 
 browser.runtime.onMessage.addListener(message =>
 {

commit c8349d82287d6364643dac1a107e9bdc8bf7c93b

Correct regression in e204c5b
Simple type (s/enabled/answer/) causing inject script to always be off.

addon/inject.js
@@ -153,8 +153,9 @@ function toggle_active(enabled)
 }
 
 browser.runtime.sendMessage({action: 'check tab enabled'})
-	.then(answer => enabled !== undefined && toggle_active(answer.enabled))
+	.then(answer => answer !== undefined && toggle_active(answer.enabled))
 	.catch(() => {});
+
 
 browser.runtime.onMessage.addListener(message =>
 {
@@ -175,4 +176,4 @@ browser.runtime.onMessage.addListener(message =>
 	}
 	else
 		return Promise.reject('Unexpected message: ' + String(message));
-})
+});