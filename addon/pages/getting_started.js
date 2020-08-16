c4fdeebb72a91a21adbe79fa3f56b147ba41a63b
Cimbali <me@cimba.li>
2020-08-16 21:32:32 +0200

    Add a preset of how vigoroulsy CleanLinks should clean links
    From the onboarding page. This should also make new user experience much
    easier. Also add the test example to the rules, so that even when
    selecting low presets, the example works.

--- addon/pages/getting_started.js
+++ addon/pages/getting_started.js
@@ -1,10 +1,14 @@
 'use strict';
 
-function update() {
-	for (const div of document.querySelectorAll('.slide')) {
-		if (window.scrollY + window.innerHeight > div.offsetTop) {
+function update_progress()
+{
+	for (const div of document.querySelectorAll('.slide'))
+	{
+		if (window.scrollY + window.innerHeight > div.offsetTop)
+		{
 			div.classList.add('visible');
-		} else {
+		} else
+		{
 			div.classList.remove('visible');
 		}
 	}
@@ -12,34 +16,55 @@ function update() {
 	document.getElementById('content').scroll({left: 0});
 
 	const n = document.querySelectorAll('.slide.visible').length;
-	[...document.querySelectorAll('#progress a')].forEach((elt, num) => {
-		if (num < n) {
+	[...document.querySelectorAll('#progress a')].forEach((elt, num) =>
+	{
+		if (num < n)
+		{
 			elt.classList.add('uncovered');
-		} else {
+		} else
+		{
 			elt.classList.remove('uncovered');
 		}
 	})
 }
 
-window.addEventListener('scroll', update)
-window.addEventListener('load', () => {
-	if (window.location.search.indexOf('update') !== -1) {
-		for (const elt of document.querySelectorAll('.update')) {
-			elt.style.display = 'block';
-		}
-	}
+const preset_choices = [
+	{auto_redir: false, drop_leaks: false, clean_headers: false, httpall:  true, only_http:  true},
+	{auto_redir: false, drop_leaks: false, clean_headers:  true, httpall:  true, only_http: false},
+	{auto_redir:  true, drop_leaks:  true, clean_headers:  true, httpall:  true, only_http: false},
+];
 
-	const list = document.getElementById('left').appendChild(document.createElement('p'));
-	list.id = 'progress';
+const preset_slider = document.getElementById('vigor');
+preset_slider.addEventListener('change', () =>
+{
+	const configuration = {...Prefs.values, ...preset_choices[document.getElementById('vigor').value]};
+	browser.storage.sync.set({configuration}).then(() =>
+	{
+		browser.runtime.sendMessage({action: 'options'});
+		Prefs.reload()
+	})
+});
 
-	for (const slide_title of document.querySelectorAll('.slide h2')) {
-		slide_title.parentNode.id = slide_title.textContent;
-		const link = list.appendChild(document.createElement('a'));
-		link.textContent = slide_title.textContent;
-		link.href = `#${encodeURIComponent(link.textContent)}`
-	}
+for (const label of document.querySelectorAll('#vigor-display label'))
+	label.addEventListener('click', evt =>
+	{
+		preset_slider.value = evt.target.getAttribute('value');
+		preset_slider.dispatchEvent(new Event('change'));
+	});
 
-	update();
-})
+const is_update = window.location.search.indexOf('update_progress') !== -1;
+for (const elt of document.querySelectorAll(is_update ? '.update_progress' : '.new-user'))
+	elt.style.display = 'block';
+
+const list = document.getElementById('progress');
+for (const slide_title of document.querySelectorAll('.slide h2'))
+{
+	const link = list.appendChild(document.createElement('a'));
+	link.textContent = slide_title.parentNode.id = slide_title.getAttribute('i18n_text');
+	link.href = `#${encodeURIComponent(link.textContent)}`
+}
 
 apply_i18n();
+
+update_progress();
+window.addEventListener('scroll', update_progress)
